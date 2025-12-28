d36c70b504417bfc31398c0fdcba67b49b4bb18c
Iow C. Five（伍庆耀） <wqy-24@outlook.com>
2025-12-28 16:28:17 +0800

    Extended syntax + Fix input
    1.Allow `let` form for its input
    2.list for input

--- ast.scm
+++ ast.scm
@@ -1,15 +1,19 @@
 (define-library (risc-v ast)
  (import (scheme base) (scheme case-lambda))
- (export ast->three)
+ (export (rename realexp ast->three))
  (begin
   (define counter 0)
+  (define (expand? arg)
+   (and (pair? arg) (not (symbol=? (car arg) 'let))))
   (define (gen-sym)
    (string->symbol (number->string (begin (set! counter (+ counter 1)) counter))))
   (define ast->three
     (case-lambda
-     [(fn arg) (if (pair? arg) (let [[t (gen-sym)]] (list 'let t (apply ast->three arg) (list fn t))) (list fn arg))]
+     [(fn arg) (if (expand? arg) (let [[t (gen-sym)]] (list 'let t (apply ast->three arg) (list fn t))) (list fn arg))]
      [(fn arg1 arg2)
       (cond
-       [(pair? arg1) (let [[t (gen-sym)]] (list 'let t (apply ast->three arg1) (ast->three fn t arg2)))]
-       [(pair? arg2) (let [[t (gen-sym)]] (list 'let t (apply ast->three arg2) (list fn arg1 t)))]
-       [else (list fn arg1 arg2)])]))))
+       [(expand? arg1) (let [[t (gen-sym)]] (list 'let t (apply ast->three arg1) (ast->three fn t arg2)))]
+       [(expand? arg2) (let [[t (gen-sym)]] (list 'let t (apply ast->three arg2) (list fn arg1 t)))]
+       [else (list fn arg1 arg2)])]
+     [(fn arg1 arg2 arg3) (if (symbol=? fn 'let) (list fn arg1 arg2 arg3) (error "Output syntax error" "The only 3-arg form is `let'"))]))
+  (define (realexp obj) (apply ast->three obj))))
