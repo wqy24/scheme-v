ca062b97331b45a9e1057bad7679ab24585cbe31
Iow C. Five（伍庆耀） <wqy-24@outlook.com>
2025-12-28 15:42:43 +0800

    Fix typo + rewrite gensym
    `ast->three` in export is misspelled
    `gensym` is not defined in `(scheme base)` so I wrote one

--- ast.scm
+++ ast.scm
@@ -1,12 +1,15 @@
 (define-library (risc-v ast)
  (import (scheme base) (scheme case-lambda))
- (export (ast->three))
+ (export ast->three)
  (begin
+  (define counter 0)
+  (define (gen-sym)
+   (string->symbol (number->string (begin (set! counter (+ counter 1)) counter))))
   (define ast->three
-   (case-lambda
-    [(fn arg) (if (pair? arg) (let [[t (gensym)]] (list 'let t (apply ast->three arg) (list fn t))) (list fn arg))]
-    [(fn arg1 arg2)
-     (cond
-      [(pair? arg1) (let [[t (gensym)]] (list 'let t (apply ast->three arg1) (ast->three fn t arg2)))]
-      [(pair? arg2) (let [[t (gensym)]] (list 'let t (apply ast->three arg2) (list fn arg1 t)))]
-      [else (list fn arg1 arg2)])]))))
+    (case-lambda
+     [(fn arg) (if (pair? arg) (let [[t (gen-sym)]] (list 'let t (apply ast->three arg) (list fn t))) (list fn arg))]
+     [(fn arg1 arg2)
+      (cond
+       [(pair? arg1) (let [[t (gen-sym)]] (list 'let t (apply ast->three arg1) (ast->three fn t arg2)))]
+       [(pair? arg2) (let [[t (gen-sym)]] (list 'let t (apply ast->three arg2) (list fn arg1 t)))]
+       [else (list fn arg1 arg2)])]))))
