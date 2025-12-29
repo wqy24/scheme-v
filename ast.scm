33c38f826e349b294c1dadf51f385ca7d44f676f
Iow C. Five（伍庆耀） <as2734@126.com>
2025-12-29 20:25:59 +0800

    Fix indent

--- ast.scm
+++ ast.scm
@@ -6,12 +6,12 @@
   (define (gen-sym)
    (string->symbol (number->string (begin (set! counter (+ counter 1)) counter))))
   (define ast->three
-    (case-lambda
-     [(fn arg) (if (pair? arg) (let [[t (gen-sym)]] (list 'let t (apply ast->three arg) (list fn t))) (list fn arg))]
-     [(fn arg1 arg2)
-      (cond
-       [(pair? arg1) (let [[t (gen-sym)]] (list 'let t (apply ast->three arg1) (ast->three fn t arg2)))]
-       [(pair? arg2) (let [[t (gen-sym)]] (list 'let t (apply ast->three arg2) (list fn arg1 t)))]
-       [else (list fn arg1 arg2)])]
-     [(fn arg1 arg2 arg3) (if (symbol=? fn 'let) (list fn arg1 (realexp arg2) (realexp arg3)) (error "Output syntax error" "The only 3-arg form is `let'"))]))
+   (case-lambda
+    [(fn arg) (if (pair? arg) (let [[t (gen-sym)]] (list 'let t (apply ast->three arg) (list fn t))) (list fn arg))]
+    [(fn arg1 arg2)
+     (cond
+      [(pair? arg1) (let [[t (gen-sym)]] (list 'let t (apply ast->three arg1) (ast->three fn t arg2)))]
+      [(pair? arg2) (let [[t (gen-sym)]] (list 'let t (apply ast->three arg2) (list fn arg1 t)))]
+      [else (list fn arg1 arg2)])]
+    [(fn arg1 arg2 arg3) (if (symbol=? fn 'let) (list fn arg1 (realexp arg2) (realexp arg3)) (error "Output syntax error" "The only 3-arg form is `let'"))]))
   (define (realexp obj) (if (pair? obj) (apply ast->three obj) obj))))
