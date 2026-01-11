eac7595902e2fd8131212218f00cb4a23083437d
Iow C. Five（伍庆耀） <wqy-24@outlook.com>
2026-01-11 16:20:39 +0800

    Correct three->three-using

--- reg-three.scm
+++ reg-three.scm
@@ -1,20 +1,23 @@
 (define-library (scheme-v reg-three)
- (import (scheme base) (scheme cxr) (scheme list))
+ (import (scheme base) (scheme cxr) (scheme list) (scheme write))
  (export three->reg-three)
  (begin
   (define (get-usings obj)
-   (if (symbol=? (car obj) 'let-using) (cadr obj) (filter symbol? (cdr obj))))
+   (cond
+    [(not (pair? obj)) '()]
+    [(symbol=? (car obj) 'let-using) (cadr obj)]
+    [else (filter symbol? (cdr obj))]))
   (define (three->three-using obj)
-   (if (atom? obj) (error "Syntax error" "Recieved atom in three->three-using"))
-   (if (symbol=? (car obj) 'let)
-    (let [[prog (caddr obj)]]
+   (if (and (pair? obj) (symbol=? (car obj) 'let))
+    (let [[prog (cadddr obj)]]
      (if (pair? prog)
       (let [[trf-prog (three->three-using prog)]]
-       (if (symbol=? (car trf-prog) 'let-using)
-        (if (memq (cadr obj) (cadr trf-prog))
-         (let [[trf-def (three->three-using (cadr obj))]]
-          (list 'let-using (append (get-usings trf-prog) (get-usings trf-def))))
-         trf-prog) ;# (let-using (symbol ...) name def prog)
-        (trf-prog)))
+       (if (memq (cadr obj) (get-usings trf-prog))
+        (let [[trf-def (three->three-using (caddr obj))]]
+         (list
+          'let-using
+          (append (get-usings trf-prog) (get-usings trf-def))
+          (cadr obj) trf-def trf-prog))
+        trf-prog))
       prog))
     obj))))
