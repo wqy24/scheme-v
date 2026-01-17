5e8cda8f78923425118763dd7d9b39848d30faab
Iow C. Five（伍庆耀） <wqy-24@outlook.com>
2026-01-17 15:43:11 +0800

    Add untested three-using->three-replacing
    Additional Changes:
    - Change R7RS large library name `(scheme list)` to `(srfi 1)`
    - Add blanks between defines

--- reg-three.scm
+++ reg-three.scm
@@ -1,12 +1,14 @@
 (define-library (scheme-v reg-three)
- (import (scheme base) (scheme cxr) (scheme list) (scheme write))
+ (import (scheme base) (scheme cxr) (srfi 1))
  (export three->reg-three)
  (begin
+
   (define (get-usings obj)
    (cond
     [(not (pair? obj)) '()]
     [(symbol=? (car obj) 'let-using) (cadr obj)]
     [else (filter symbol? (cdr obj))]))
+
   (define (three->three-using obj)
    (if (and (pair? obj) (symbol=? (car obj) 'let))
     (let [[prog (cadddr obj)]]
@@ -20,4 +22,22 @@
           (cadr obj) trf-def trf-prog))
         trf-prog))
       prog))
-    obj))))
+    obj))
+
+  (define (three-using->three-replacing obj)
+   (define (recur obj symlist)
+    (if (and (pair? obj) (symbol=? (car obj) 'let-using))
+     (begin
+      (if (apply or (map (lambda (x) (not (memq x symlist))) (cadr obj)))
+       (error "Internal Transferring Exception" "Using symbol that doesn't exist in three-using->three-replacing"))
+      (let* [[replacables (filter (lambda (x) (not (memq x (cadr obj)))) symlist)]
+             [replaced (if (null? replacables) #f (car replacables))]
+             [new-symlist (cons (cadr obj) (remove (lambda (x) (and replaced (symbol=? x replaced))) symlist))]]
+       (list
+        'let-replacing
+        replaced
+        (caddr obj) 
+        (recur (cadddr obj) new-symlist)
+        (recur (car (cddddr obj) new-symlist)))))
+     obj))
+   (recur obj '()))))
