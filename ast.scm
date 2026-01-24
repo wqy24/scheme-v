d0c95fdd9bf80b70eec9df6e6d9cf3a77c0521da
wqy24 <wqy-24@outlook.com>
2026-01-24 11:37:48 +0800

    Correct implementation for three->ast

--- ast.scm
+++ ast.scm
@@ -1,23 +1,23 @@
 (define-library (scheme-v ast)
- (import (scheme base) (scheme case-lambda) (srfi 1))
- (export (rename realexp ast->three))
+ (import (scheme base) (srfi 1))
+ (export ast->three)
  (begin
   (define counter 0)
   (define (gen-sym)
-   (string->symbol (append "wqy24's scheme-v - internal temperony symbol" (number->string (begin (set! counter (+ counter 1)) counter)))))
+   (string->symbol (string-append "scheme-v's temporany symbol for `let' internal expression" (number->string (begin (set! counter (+ counter 1)) counter)))))
   (define (ast->three obj)
    (if (pair? obj)
-    (let* [[bindings (map (lambda (x) (cons (filter pair? obj) (gen-sym))))]
-           [bindings-x bindings]
-           [t-obj (map (lambda (x)
-                        (if (pair? x)
-                         (let [[s (cdar bindings-x)]]
-                          (set! bindings-x (cdr bindings-x))
-                          s)))
-                       obj)]]
+    (let* [[bindings (map (lambda (x) (cons x (gen-sym))) (filter pair? obj))]
+           [t-obj (let loop [[bindings-x bindings]
+                             [t-obj obj]]
+                   (if (pair? t-obj)
+                    (if (pair? (car t-obj))
+                     (cons (cdar bindings-x) (loop (cdr bindings-x) (cdr t-obj)))
+                     (cons (car t-obj) (loop bindings-x (cdr t-obj))))
+                    t-obj))]]
      (let loop [[bindings-2 bindings]
                 [tt-obj t-obj]]
       (if (null? bindings-2)
        tt-obj
-       (loop (cdr bindings-2) (list 'let (cdar bindings-2) (caar bindings-2) tt-obj)r))))
+       (loop (cdr bindings-2) (list 'let (cdar bindings-2) (ast->three (caar bindings-2)) tt-obj)))))
     obj))))
